4bc899c46abe94f4f4e518573665ace6b84ec4d8
Shogo Takata <peshogo+github.com@gmail.com>
2021-12-25 18:33:14 +0900

    http server

--- 7-guessing-game/game/src/main.rs
+++ 7-guessing-game/game/src/main.rs
@@ -1,6 +1,6 @@
 use rand::Rng;
+use read_input::prelude::*;
 use std::cmp::Ordering;
-use std::io;
 
 fn main() {
     println!("Guess the number!");
@@ -10,11 +10,7 @@ fn main() {
     loop {
         println!("Please input your guess.");
 
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+        let mut guess: String = input().get();
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
---
+++ README.md
@@ -0,0 +1,1 @@
+# Rustハンズオン-MCCMMANCCレクリエーション
